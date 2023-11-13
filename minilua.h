d488a7724d8db1f745979843bc8590308b573387
David Reid <mackron@gmail.com>
2023-11-14 07:53:09 +1000

    Namespace a conflicting symbol in the implementation.

--- minilua.h
+++ minilua.h
@@ -21380,12 +21380,12 @@ LUALIB_API char *luaL_buffinitsize (lua_State *L, luaL_Buffer *B, size_t sz) {
 */
 
 /* index of free-list header (after the predefined values) */
-#define freelist	(LUA_RIDX_LAST + 1)
+#define l_freelist	(LUA_RIDX_LAST + 1)
 
 /*
 ** The previously freed references form a linked list:
-** t[freelist] is the index of a first free index, or zero if list is
-** empty; t[t[freelist]] is the index of the second element; etc.
+** t[l_freelist] is the index of a first free index, or zero if list is
+** empty; t[t[l_freelist]] is the index of the second element; etc.
 */
 LUALIB_API int luaL_ref (lua_State *L, int t) {
   int ref;
@@ -21394,19 +21394,19 @@ LUALIB_API int luaL_ref (lua_State *L, int t) {
     return LUA_REFNIL;  /* 'nil' has a unique fixed reference */
   }
   t = lua_absindex(L, t);
-  if (lua_rawgeti(L, t, freelist) == LUA_TNIL) {  /* first access? */
+  if (lua_rawgeti(L, t, l_freelist) == LUA_TNIL) {  /* first access? */
     ref = 0;  /* list is empty */
     lua_pushinteger(L, 0);  /* initialize as an empty list */
-    lua_rawseti(L, t, freelist);  /* ref = t[freelist] = 0 */
+    lua_rawseti(L, t, l_freelist);  /* ref = t[l_freelist] = 0 */
   }
   else {  /* already initialized */
     lua_assert(lua_isinteger(L, -1));
-    ref = (int)lua_tointeger(L, -1);  /* ref = t[freelist] */
+    ref = (int)lua_tointeger(L, -1);  /* ref = t[l_freelist] */
   }
   lua_pop(L, 1);  /* remove element from stack */
   if (ref != 0) {  /* any free element? */
     lua_rawgeti(L, t, ref);  /* remove it from list */
-    lua_rawseti(L, t, freelist);  /* (t[freelist] = t[ref]) */
+    lua_rawseti(L, t, l_freelist);  /* (t[l_freelist] = t[ref]) */
   }
   else  /* no free elements */
     ref = (int)lua_rawlen(L, t) + 1;  /* get a new reference */
@@ -21418,11 +21418,11 @@ LUALIB_API int luaL_ref (lua_State *L, int t) {
 LUALIB_API void luaL_unref (lua_State *L, int t, int ref) {
   if (ref >= 0) {
     t = lua_absindex(L, t);
-    lua_rawgeti(L, t, freelist);
+    lua_rawgeti(L, t, l_freelist);
     lua_assert(lua_isinteger(L, -1));
-    lua_rawseti(L, t, ref);  /* t[ref] = t[freelist] */
+    lua_rawseti(L, t, ref);  /* t[ref] = t[l_freelist] */
     lua_pushinteger(L, ref);
-    lua_rawseti(L, t, freelist);  /* t[freelist] = ref */
+    lua_rawseti(L, t, l_freelist);  /* t[l_freelist] = ref */
   }
 }
 
